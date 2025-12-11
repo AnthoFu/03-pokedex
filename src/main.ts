207099f06cbbcf2a1b836179af976d1b19f24312
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-12-11 11:00:15 -0400

    [V.0.1.3] feat: Agregada la logica para la inserccion de pokemon
    (Tambien se agregaron los paquetes class-validator y class-transformer)

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v2')
 
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true,
+    })
+  )
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
