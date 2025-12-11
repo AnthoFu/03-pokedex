fe088b1380b73ba8c97173f41bc6e70f95994de0
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2025-12-11 09:58:49 -0400

    [V.0.1.2] feat: agregado el PokemonSchema para mongoDB

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,26 @@
-export class Pokemon {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+// Volvemos esta clase un esquema
+@Schema()
+export class Pokemon extends Document{ // Aqui extendemos documento de mongoose para utilizar sus funciones
+
+
+    // Tambien tenemos un id pero eso nos los da mongo
+
+    // Le agregamos las propiedades al atributo name
+    @Prop({
+        unique: true,
+        index:true
+    })
+    name:string;
+
+    // Igual al number
+    @Prop({
+        unique: true,
+        index:true
+    })
+    no:number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass( Pokemon );
