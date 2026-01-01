a5e4c27d308191b86c3ed6668c49bba931cc2ed2
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-01 19:35:14 -0400

    [V0.1.12] Feat: Agregada la funcion inicial del metodo /seed para la obtencion de pokemones directamente desde la PokeAPI

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+
+@Injectable()
+export class SeedService {
+
+  private readonly axios: AxiosInstance = axios;
+
+
+  async executeSeed(){
+    
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=100');
+
+    data.results.forEach(({name, url}) => {
+      const segment = url.split('/');
+      const no:number = +segment [ segment.length - 2 ];
+
+      console.log ('[Pokemon obtenido mediante /seed]:', name, no);
+    } )
+
+    return data.results;
+  }
+}
