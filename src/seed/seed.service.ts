beef3e71055b206b3dff2e5646caf9a0ad69634f
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-06 14:57:58 -0400

    [V0.2.1] feat: Agregado parametros para el endpoint GET /pokemon y arreglado el bug de borrado de datos de GET /seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -20,24 +20,27 @@ export class SeedService {
     ){}
 
 
-  async executeSeed(){
-    
-    const data  = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=100');
+  async executeSeed() {
 
-    await this.pokemonModel.deleteMany({}); // delete * from pokemons
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
 
-    const pokemonToInsert: {name: string, no: number} [] = [];
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
-    data.results.forEach(async ({name, url}) => {
-      const segment = url.split('/');
-      const no:number = +segment [ segment.length - 2 ];
-      // const pokemon = await this.pokemonModel.create({name, no});
-      // console.log ('[Pokemon obtenido mediante /seed]:', name, no);
-      pokemonToInsert.push();
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2 ];
+
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no }); // [{ name: bulbasaur, no: 1 }]
+
     });
 
     await this.pokemonModel.insertMany(pokemonToInsert);
 
-    return 'Seed ejecutado con exito';
+
+    return 'Seed ejecutada con exito';
   }
 }
