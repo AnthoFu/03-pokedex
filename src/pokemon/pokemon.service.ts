423b96969d121b440fdd58f8e4d0889a969181a0
Anthony Fuentes <anthony.fuentes2005@gmail.com>
2026-01-06 17:17:34 -0400

    [V0.2.5] feat: Implementada carga de datos de .env

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,18 +5,23 @@ import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
 
   constructor(
     
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-    
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
 
-  ){}
+  ){
+    
+    this.defaultLimit = this.configService.get<number>('DEFAULT_LIMIT', 5);
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase(); // para poner en minusculas el nombre obtenido
@@ -32,7 +37,7 @@ export class PokemonService {
 
   findAll( paginationDto: PaginationDto ) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
       .limit( limit )
